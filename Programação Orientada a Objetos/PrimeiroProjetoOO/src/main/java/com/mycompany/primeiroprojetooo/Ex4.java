9f60a5147fc64f7793742cb48ee37e5930c2b419
Juliana Moura <juliana_moura@id.uff.br>
2022-08-27 14:12:13 -0300

    add file ex4

---
+++ Programação Orientada a Objetos/PrimeiroProjetoOO/src/main/java/com/mycompany/primeiroprojetooo/Ex4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.primeiroprojetooo;
+
+/**
+ *
+ * @author juliana
+ */
+public class Ex4 {
+    public static void main(String[] args) {
+        for(int i = 1; i<101; i++){
+            if(i%3 == 0) System.out.println(i);
+        }
+    }
+}
