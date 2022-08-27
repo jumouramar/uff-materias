9e2eb9504704fbf33c021d1aaf606a41c9e244c8
Juliana Moura <juliana_moura@id.uff.br>
2022-08-27 14:07:10 -0300

    add file ex3

---
+++ Programação Orientada a Objetos/PrimeiroProjetoOO/src/main/java/com/mycompany/primeiroprojetooo/Ex3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.primeiroprojetooo;
+
+/**
+ *
+ * @author juliana
+ */
+
+public class Ex3 {
+    public static void main(String[] args) {
+        int soma = 0;
+        for(int i = 1; i<1000; i++){
+            soma += i;
+        }
+        System.out.println("Soma: " + soma);
+    }
+}
