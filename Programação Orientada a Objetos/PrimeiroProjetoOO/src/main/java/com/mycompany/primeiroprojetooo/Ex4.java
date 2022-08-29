71de19b39d3e6e810e6a8c5e7a89a182faa870e8
Juliana Moura <juliana_moura@id.uff.br>
2022-08-29 11:37:44 -0300

    update ex4

--- Programação Orientada a Objetos/PrimeiroProjetoOO/src/main/java/com/mycompany/primeiroprojetooo/Ex4.java
+++ Programação Orientada a Objetos/PrimeiroProjetoOO/src/main/java/com/mycompany/primeiroprojetooo/Ex4.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.primeiroprojetooo;
 
-/**
- *
- * @author juliana
- */
 public class Ex4 {
     public static void main(String[] args) {
         for(int i = 1; i<101; i++){
