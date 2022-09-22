a141bd5477e4fdd2f2071ca4db6ddd334c5a5fc3
Juliana Moura <juliana_moura@id.uff.br>
2022-09-22 16:40:03 -0300

    Create maior.c

---
+++ Estrutura de Dados/Árvore Binária de Busca/Exercícios/maior.c
@@ -0,0 +1,70 @@
+/* nó que contém o maior valor de chave nessa árvore */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct sNoA {
+    int chave;
+    struct sNoA *esq;
+    struct sNoA *dir;
+} TNoA;
+
+
+TNoA *maior(TNoA *no) {
+    while(no->dir != NULL)
+        no = no->dir;
+    return no;
+}
+
+void imprime(TNoA *nodo, int tab) {
+    for (int i = 0; i < tab; i++) {
+        printf("-");
+    }
+    if (nodo != NULL) {
+        printf("%d\n", nodo->chave);
+        imprime(nodo->esq, tab + 2);
+        printf("\n");
+        imprime(nodo->dir, tab + 2);
+    } else printf("vazio");
+}
+
+TNoA *insere(TNoA *no, int chave) {
+    if (no == NULL) {
+        no = (TNoA *) malloc(sizeof(TNoA));
+        no->chave = chave;
+        no->esq = NULL;
+        no->dir = NULL;
+    } else if (chave < (no->chave))
+        no->esq = insere(no->esq, chave);
+    else if (chave > (no->chave)) {
+        no->dir = insere(no->dir, chave);
+    } else {
+        printf("Inserção inválida! ");
+        exit(1);
+    }
+    return no;
+}
+
+int main(void) {
+    TNoA *raiz, *no;
+    raiz = NULL;
+
+    char l[100];
+    char delimitador[] = "-";
+    char *ptr;
+    int valor;
+
+    scanf("%s", l);
+    ptr = strtok(l, delimitador);
+    while(ptr != NULL) {
+        valor = atoi(ptr);
+        raiz = insere(raiz, valor);
+        ptr = strtok(NULL, delimitador);
+    }
+
+    no = maior(raiz);
+    if (no != NULL) {
+        printf("%d", no->chave);
+    }
+}
