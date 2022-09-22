f13c230d82d16471cb9418496e42797067811b5f
Juliana Moura <juliana_moura@id.uff.br>
2022-09-22 15:58:22 -0300

    Create recursivo-ordem-simetrica.c

---
+++ Estrutura de Dados/Árvore Binária/Caminhamento/recursivo-ordem-simetrica.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noA {
+    char info;
+    struct noA *esq;
+    struct noA *dir;
+} TNoA;
+
+TNoA *inicializa(void) {
+    return NULL;
+}
+
+void simetrica(TNoA* a) {
+    if (a!= NULL) {
+        simetrica(a->esq);
+        printf("%c\n",a->info);
+        simetrica(a->dir);
+    }
+}
+ 
+TNoA *criaNo(char ch) {
+    TNoA *novo;
+    novo = (TNoA *) malloc(sizeof(TNoA));
+    novo->info = ch;
+    novo->esq = NULL;
+    novo->dir = NULL;
+    return novo;
+}
+
+int main(void) {
+    TNoA *raiz;
+    raiz = criaNo('A');
+    raiz->esq = criaNo('B');
+    raiz->dir = criaNo('C');
+    raiz->dir->esq = criaNo('D');
+    raiz->dir->dir = criaNo('E');
+    simetrica(raiz);
+}
